46684e2bd801afb70ba23b9e37ab8b26879e04a3
unknown <nehemiahabelcjohan@gmail.com>
2025-11-05 22:11:31 +0000

    Improved commenting of Part B

--- part_b.m
+++ part_b.m
@@ -1,7 +1,6 @@
 clear;
 clc;
 
-% QN 1
 function [fit, condition, alpha_fed] = regress(x, y, M, condition)
     % Fits and plots Mth order polynomial to data points (x, y)
 
@@ -65,6 +64,7 @@ function central_diff_error_noise = central(h, t, y_noise, y_prime)
         squared_error_noise(n-1) = (y_prime_noise(n-1) - y_prime(n)).^2;
     end
 
+    % Compare the central difference scheme with the analytical solution
     figure
     plot(t_sampled_for_deriv, y_prime_noise, 'r', t, y_prime, 'b')
     legend('central diff', 'analytical')
@@ -100,9 +100,12 @@ alphas = zeros(M, M+1);
 
 figure
 tiledlayout(5, 2)
+% Get fitted polynomial, condition number, and polynomial coefficients for
+% each proposed polynomial degree
 for m = 1:M
     [fits(m, :), condition, alphas(m, :)] = regress(t, y_noise, m, condition);
 
+    % Compare the fitted polynomial with the original noisy y curve
     nexttile
     plot(t, fits(m,:), 'r', t , y_noise, 'b')
     legend('fitted poly', 'original noisy y')
@@ -110,33 +113,40 @@ for m = 1:M
     ylabel("y")
 end
 
+% Plot condition number vs polynomial degree, log vertical axis
 figure
 semilogy(1:M, condition, '-x')
 xlabel("polynomial degree m")
 ylabel("condition number (log-axis)")
-% QN 1
 
-% QN 2
+% Initialise arrays for finding 
 diff_alphas = zeros(M, M);
 diff_fits = zeros(M, length(t));
 diff_condition = zeros(1, M);
 squared_error_fitted = zeros(M, length(t));
 diff_fitted_poly_error = zeros(1, M);
+
 figure
 tiledlayout(5, 2)
+
 for m = 1:M
+    % Differentiate the fitted polynomials
     diff_alphas(:, m) = alphas(:, m) .* (10-m+1);
     
+    % Turn the alpha matrix into a form usable for V*alpha above
     use_diff_alphas = nonzeros(diff_alphas(:, m));
     
+    % Find the fitted polynomial derivatives and their condition numbers
     [diff_fits(m, :), diff_condition(m)] = plt(t, use_diff_alphas);
 
+    % Find the RMS/L2 Error
     for i = 1:length(t)
         squared_error_fitted(m, i) = (diff_fits(m, i) - y_prime(i)).^2;
     end
-
     diff_fitted_poly_error(1, m) = rms_error(squared_error_fitted(m, 15:end-15), t(15:end-15));
 
+    % Compare derivative retrieved by polynomial fitting and the analytical
+    % derivative
     nexttile
     plot(t, diff_fits(m, :), 'r', t, y_prime, 'b')
     legend('via poly fitting', 'analytical soln')
@@ -150,5 +160,3 @@ figure
 plot(1:M, diff_fitted_poly_error, '-x')
 xlabel("polynomial degree m")
 ylabel("RMS Error")
-
-% QN 2
